09fbef777da1b3331d7b062219a9a4ed1b28ecf4
3sem <31904035+3sem@users.noreply.github.com>
2023-02-17 14:59:58 +0300

    Update odg_tools.py

--- odg_tools.py
+++ odg_tools.py
@@ -12,24 +12,37 @@ class ODGMakingMode(Enum):
     FULLYCONNECTED = 0
     STOPLIST = 1
     ALLOWLIST = 2
+    FROMPATHS = 3
 
 
 class ODG:
     def __init__(self, fullseq, mode=ODGMakingMode.FULLYCONNECTED):
         print(sorted(nx.complete_graph(len(fullseq), nx.DiGraph())))
         if mode == ODGMakingMode.FULLYCONNECTED:
-            mapping = {}
-            for i, item in enumerate(fullseq):
-                mapping.update({i: item})
-            self.G = nx.relabel_nodes(nx.complete_graph(len(fullseq), nx.DiGraph()), mapping)
-
+            self.G = nx.complete_graph(len(fullseq), nx.DiGraph())
+            for n in self.G.nodes:
+                self.G.nodes[n].update({'passname': fullseq[n]})
+            nx.set_edge_attributes(self.G, 1, 'weight')
         elif mode == ODGMakingMode.STOPLIST:
             pass
         elif mode == ODGMakingMode.ALLOWLIST:
             pass
+        elif mode == ODGMakingMode.FROMPATHS:
+            pass
         else:
             print("Unknown graph making mode")
 
-    def dump(self):
-        nx.draw(self.G)
+    def plot(self):
+        pos = nx.spring_layout(self.G)
+        nx.draw(self.G, pos)
+        node_labels = nx.get_node_attributes(self.G, 'passname')
+        nx.draw_networkx_labels(self.G, pos, labels=node_labels)
+        edge_labels = nx.get_edge_attributes(self.G, 'weight')
+        nx.draw_networkx_edge_labels(self.G, pos, edge_labels)
         plt.show()
+
+        
+if __name__ == '__main__':
+    odg = ODG(['A', 'B', 'C'])
+    odg.plot()
+    print(odg.G.edges(data=True))
