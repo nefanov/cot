c551d013200a04546525fcc3b2062137bb0ff10c
3sem <31904035+3sem@users.noreply.github.com>
2023-02-19 21:00:07 +0300

    Update cgym_tools.py

--- cgym_tools.py
+++ cgym_tools.py
@@ -12,7 +12,7 @@ def test_packages_integrity():
     env = compiler_gym.make(  # creates a new environment (same as gym.make)
         "llvm-v0",  # selects the compiler to use
         benchmark="cbench-v1/qsort",  # selects the program to compile
-        observation_space="Ir2vecFlowAware",
+        observation_space="Ir2vecFlowAware",  # selects the observation space
         reward_space="IrInstructionCountOz",  # selects the optimization target
     )
     env.reset()  # starts a new compilation session
@@ -28,5 +28,28 @@ def test_packages_integrity():
     print(done)
     env.close()
 
+def test_cycle():
+    print("=====TEST #2=====")
+    env = compiler_gym.make(  # creates a new environment (same as gym.make)
+        "llvm-v0",  # selects the compiler to use
+        benchmark="cbench-v1/qsort",  # selects the program to compile
+        observation_space="Ir2vecFlowAware",  # selects the observation space
+        reward_space="IrInstructionCountOz",  # selects the optimization target
+    )
+    env.reset()  # starts a new compilation session
+    episode_reward = 0
+
+    for i in range(1, 201):
+        action = env.action_space.sample()
+        #print("A", action)
+        print("Action  #", action, ":", env.action_spaces[0].names[int(action.__repr__())])
+        observation, reward, done, info = env.step(action)
+        if done:
+            break
+        episode_reward += reward
+        print(f"Step {i}, quality={episode_reward:.3%}")
+
+
 if __name__ == '__main__':
     test_packages_integrity()
+    print("Test 100 times Ir2Vec")
