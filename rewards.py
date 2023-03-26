4c994d8119afdeaee8e0e6d1778e74073efc6768
= <=>
2023-03-26 11:32:56 +0300

    rewards: const_threshold fix

--- rewards.py
+++ rewards.py
@@ -14,30 +14,24 @@ def delta(baseline, measurement, normalizer=None):
 
 """
 const_factor reward is floating-point value of metric m which should be minimized scaled by the metric n, 
-deviation from which baseline upwards is undesirable, but downwards is good: 
-Let f(n) is (baseline(n)-measured(n))/baseline(n), d(m) = (baseline(m)-measured(m))/1.
-
-(1+f(n))*d(m), if d(m) > 0 and ( f(n) > 0 or (f(n) <= 0 and |f(n)| < threshold))
-  (1-f(n))*d(m), if d(m) <= 0
-  else 0.
-,
- 
- thus, for downward deviation, get the increasing of reward(m),
-else -- decreasing. Increasing/decreasing are uniform as percentage of deviation, but thresholded in goal of n fixing.
+deviation from which baseline upwards is undesirable, but downwards is good.
+Increasing/decreasing are uniform as percentage of deviation, but thresholded in goal of n fixing.
 if threshold is set and upward deviation is bigger than threshold, and m is worst than its baseline, 
 """
 
 
-def const_factor_threshold(baseline_m, measured_m, baseline_n, measured_n, threshold=0.01, m_norm = None):
+def const_factor_threshold(baseline_m, measured_m, baseline_n, measured_n, threshold=0.01, m_norm=None):
+    # constrained parameter gain
     f_n = (baseline_n - measured_n) / baseline_n
+    # argmin parameter gain
     d = (baseline_m - measured_m)
     if not m_norm:
-        d=d/baseline_m
+        d = d / baseline_m
     else:
-        d=d/m_norm
-    if d > 0:
-        if (f_n > 0) or (f_n <= 0 and math.fabs(f_n) < threshold):
-            return (1.+f_n) * d
+        d = d / m_norm
+    if d >= 0: # positive result: argmin parameter decreasing
+        if (f_n > 0) or (f_n <= 0 and math.fabs(f_n) < threshold):  # if const param increased only a little, it is OK
+            return (1. + f_n) * d
         else:
             return 0.
     else:
