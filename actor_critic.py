036bd1841816953b5dd98237b95aff84cea26a24
name <31904035+3sem@users.noreply.github.com>
2023-03-31 20:00:19 +0300

    Update actor_critic.py

--- actor_critic.py
+++ actor_critic.py
@@ -50,7 +50,7 @@ flags.update({"log_mode": LogMode.SHORT})
 flags.update({"logger": Logger(flags["log_mode"])})
 flags.update({"actions_white_list": None}) # by default (if None), all actions from any action space are possible
 flags.update({"patience": 5})  # patience for steps with no positive reward
-flags.update({"search_iterations": 2})  # for non-RL search it is required dramatically more steps
+flags.update({"search_iterations": 100})  # for non-RL search it is required dramatically more steps
 FLAGS = flags
 
 eps = np.finfo(np.float32).eps.item()
@@ -320,7 +320,7 @@ def single_pass_eval(env, reward_estimator=const_factor_threshold,
         action = v
         state, r, d, _ = env.step(v)
         reward = reward_estimator(env.hetero_os_baselines[0], state[1], reward_if_list_func(env.hetero_os_baselines[1]), reward_if_list_func(state[2]), prev_size,prev_runtime)
-        print("Action", env.action_spaces[0].names[action], "R", reward, "; size:",prev_size,"->", state[1], "; runtime:", prev_runtime,"->", reward_if_list_func(state[2]))
+        print("Action", env.action_spaces[0].names[action], "R", reward, "; size:", prev_size, "->", state[1], "; runtime:", prev_runtime, "->", reward_if_list_func(state[2]))
 
 
 def one_pass_perform(env, prev_state, action, reward_estimator=const_factor_threshold, reward_if_list_func=lambda a: np.mean(a)):
@@ -356,6 +356,19 @@ def examine_each_action(env, state, reward_estimator=const_factor_threshold, rew
             passes_results.append(one_pass_perform(copy_env, state, v, reward_estimator=reward_estimator, reward_if_list_func=reward_if_list_func))
     return passes_results
 
+"""
+this is very slow (linear) implementation of max subseq search from start element
+"""
+
+def max_subseq_from_start(seq: list, episode_reward=0.) -> list:
+    gain = episode_reward
+    up_lim = len(seq) - 1
+    for i in range(len(seq)):
+        S = sum([s['size gain %'] for s in seq[:i]])
+        if S > gain:
+            gain = S
+            up_lim = i
+    return seq[:up_lim]
 
 def search_strategy_eval(env, reward_estimator=const_factor_threshold,
                          reward_if_list_func=lambda a: np.mean(a),
@@ -386,6 +399,8 @@ def search_strategy_eval(env, reward_estimator=const_factor_threshold,
 
     print("====================================================")
     pprint.pprint(action_log)
+    # find the subsequence from start, which gives max size gain
+    action_log = max_subseq_from_start(action_log, episode_reward=episode_size_gain)
     return {"action_log": action_log, "episode_reward": episode_reward, "episode_size_gain": episode_size_gain}
 
 
