e4cfa043fcf464cb16f28398c2b558263c19feb4
name <31904035+3sem@users.noreply.github.com>
2023-03-31 11:42:40 +0300

    Update search_policies.py

--- search_policies.py
+++ search_policies.py
@@ -1,3 +1,7 @@
+import random
+
+random.seed(0)
+
 def pick_max_size_gain(results: list) -> list:
     results = sorted(results, key=lambda d: d['size gain %'])
     return results
@@ -8,3 +12,11 @@ def pick_all_positive_size_gain(results: list) -> list:
     if results[-1]['size gain %'] <= 0:
         return [results[-1]]
     return [item for item in results if item['size gain %'] > 0]
+
+
+def pick_random_from_positive(results: list) -> list:
+    return [random.choice(pick_all_positive_size_gain(results))]
+
+
+def pick_random(results: list) -> list:
+    return [random.choice(results)]
