c97edcd15fea5b7350522bb740bda6c0a76b6749
= <=>
2023-04-02 13:14:36 +0300

    search_policies **kwargs extension

--- search_policies.py
+++ search_policies.py
@@ -2,21 +2,21 @@ import random
 
 random.seed(0)
 
-def pick_max_size_gain(results: list) -> list:
+def pick_max_size_gain(results: list, **kwargs) -> list:
     results = sorted(results, key=lambda d: d['size gain %'])
     return results
 
 
-def pick_all_positive_size_gain(results: list) -> list:
+def pick_all_positive_size_gain(results: list, **kwargs) -> list:
     results = sorted(results, key=lambda d: d['size gain %'])
     if results[-1]['size gain %'] <= 0:
         return [results[-1]]
     return [item for item in results if item['size gain %'] > 0]
 
 
-def pick_random_from_positive(results: list) -> list:
+def pick_random_from_positive(results: list, **kwargs) -> list:
     return [random.choice(pick_all_positive_size_gain(results))]
 
 
-def pick_random(results: list) -> list:
+def pick_random(results: list, **kwargs) -> list:
     return [random.choice(results)]
