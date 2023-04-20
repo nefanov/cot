8d11840cbc8e63bd2c7db2b4692c77f52b9f69ed
name <31904035+3sem@users.noreply.github.com>
2023-04-20 19:32:41 +0300

    Update gcc_env.py

--- gcc/gcc_env.py
+++ gcc/gcc_env.py
@@ -1,16 +1,23 @@
 import os
+import re
+import subprocess
 from gcc_reward import *
+
+from common import FLAGS, printRed, printLightPurple, printGreen, printYellow
+
+FLAGS['tmpdir'] = "/home/nefanov/compiler_experiments/cot_contrib"
 
 
 class gcc_benchmark:
-    def __init__(self, from_dict={}):
+    def __init__(self, from_dict={}, tmpdir=FLAGS['tmpdir']):
         self.content = from_dict
         self.compile_cmds = list()
         self.pre_compile_cmds = list()
         self.run_cmd = list()
-        self.filepath = None
+        self.filepaths = list()
         self.outfile = list()
         self.timeout_seconds = None
+        self.log_file_path = tmpdir + os.sep + "last_compile_log.txt"
 
     def add_compile_cmd(self):
         pass
@@ -18,18 +25,20 @@ class gcc_benchmark:
     def add_run_cmd(self):
         pass
 
-    def make_benchmark(self, tmpdir, name="program.c", run_args=list(),
+    def make_benchmark(self, tmpdir, names=["program.c"], run_args=list(),
                     run_timeout_seconds=10,
                     extra_objects_list=None,
                     extra_include_dirs=None,
-                    sys_settings=dict()
+                    sys_settings=dict(),
                        ):
         arch = "native"
-        compiler = sys_settings.get('compiler', "$CC")
-        inputs = sys_settings.get('inputs', "$IN")
+        compiler = sys_settings.get('compiler', "gcc")
         output_bin = sys_settings.get('output_bin', "a.out")
-        sys_lib_flags = sys_settings.get('sys_lib_flags', self.get_system_library_flags())
+        sys_lib_flags = sys_settings.get('sys_lib_flags', [])
         extra_obj = extra_objects_list if extra_objects_list else list()
+        extra_compiler_flags = sys_settings.get('extra_compiler_flags', [])
+
+        self.tmpdir = tmpdir
 
         if 'arch_triplet' in sys_settings.keys():
             if sys_settings['arch_triplet'].startswith("aarch64"):
@@ -40,11 +49,12 @@ class gcc_benchmark:
                 sys_lib_flags.append('-isystem')
                 sys_lib_flags.append(item)
 
-        self.filepath = tmpdir + os.sep + name
-        self.compile_cmds.extend(
-            [compiler, inputs] + extra_obj + sys_lib_flags
-        )
-        self.outfile.extend([output_bin])
+        for name in names: # name is relative path to TU from the tmp dir
+            self.filepaths.append([tmpdir + os.sep + nm for nm in name.split(" ")])
+            self.compile_cmds.append(
+                [compiler] + [" ".join(self.filepaths[-1])] + extra_obj + sys_lib_flags + extra_compiler_flags
+            )
+        self.outfile.extend([output_bin]) # by default, there are only one running artifact
         self.timeout_seconds = run_timeout_seconds
         if arch == "native":
             self.run_cmd.extend(["./" + output_bin] + run_args)
@@ -53,14 +63,99 @@ class gcc_benchmark:
                 ["qemu-aarch64 -L " + sys_settings['target_libs_dir'] + " ./" + output_bin] + run_args)
 
 
+    def compile(self, opt=None):
+        for cmd in self.compile_cmds:
+            if opt:
+                cmd.append(" ".join(opt))
+            results = subprocess.run([" ".join(cmd)], text=True, shell=True, capture_output=True)
+            with open(self.log_file_path, 'w+') as fp:
+                output = "Building by cmd: " + str(cmd) + ":\n" + "STDOUT: " + str(results.stdout) + "\nSTDERR: " + str(results.stderr)
+                fp.write(output)
+                print(output)
+            if (results.returncode != 0):
+                return 1, cmd
+        return 0, cmd
+
+    def run(self, pre_cmd=["time"], need_compile=True):
+        if need_compile is True:
+            compile_ret = self.compile()
+            if compile_ret[0] != 0:
+                pass
+                printRed("Compile error on cmd: " + str(compile_ret[1]))
+                return 1
+            else:
+                printGreen("Compile success.")
+
+        results = subprocess.run([" ".join(pre_cmd + self.run_cmd)], text=True, shell=True, capture_output=True)
+        with open(self.log_file_path, 'w+') as fp:
+            output = "Run by cmd: " + str(pre_cmd + self.run_cmd) + ":\n" + "STDOUT: " + str(
+                results.stdout) + "\nSTDERR: " + str(
+                results.stderr)
+            fp.write(output)
+            print(output)
+
+        if results.returncode != 0:
+            printRed("Run failed.")
+            return 1, 0.
+        else:
+            printGreen("Run success.")
+            out = results.stdout
+            if "time" in pre_cmd:
+                rt = re.findall(r'[0-9]+.[0-9]+elapsed', results.stderr)
+                return 0, float((lambda text, suffix:
+                       text[:-len(suffix)] if text.endswith(suffix) and len(suffix) != 0 else text)(rt[0], "elapsed"))
+            else:
+                return 0, 0.
+
+
 class gcc_env:
-    def __init__(self):
-        pass
-    def make(  # creates a new environment (same as gym.make)
+    def __init__(self,  # creates a new environment (same as gym.make)
         config = dict(),  # selects the compiler to use
-        benchmark=None,  # selects the program to compile
-        observation_space=gcc_benchmark(),  # selects the observation space
-        reward_space="ObjectTextSizeBytes",  # selects the optimization target
-    ):
+        benchmark=gcc_benchmark(),  # selects the program to compile
+        observation_space=None,  # selects the observation space
+        reward_spaces=[],  # selects the optimization target
+        action_space=list()):
+        self.action_history = list()
+        self.action_space = action_space
+        self.benchmark = benchmark
+        self.observation_space=observation_space
+        self.config=config
+        self.reward_spaces = reward_spaces
+
         pass
-      
+
+
+    def reset(self):
+        self.benchmark.compile()
+        self.action_history.clear()
+        pass
+
+
+    def step(self, action):
+        result = self.multistep([action])
+        return result
+
+
+    def multistep(self, actions:list, reward_func = lambda l, hdrs: np.mean(l)):
+        state = None
+        done = False
+        info = None
+
+        seq = self.action_history + actions
+        self.benchmark.compile(opt=seq)
+        reward_metrics = list()
+        for rw_meter in self.reward_spaces:
+            reward_metrics.append(rw_meter.evaluate(env=self))
+
+        reward = reward_func(reward_metrics, [r.kind for r in self.reward_spaces])
+        self.action_history + actions
+        return state, reward, done, info
+
+
+if __name__ == '__main__':
+    gbm = gcc_benchmark()
+    gbm.make_benchmark(tmpdir=FLAGS['tmpdir'])
+    #print(gbm.run(need_compile=False))
+    env = gcc_env(benchmark=gbm, reward_spaces=[RuntimeRewardMetrics()])
+    env.reset()
+    print(env.step(action="-ftree-vectorize"))
